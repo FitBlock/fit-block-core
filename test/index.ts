1892b13444ec47b27ec2d94b6d4e0f4e9beb12b0
zy445566 <zyct@vip.qq.com>
2020-01-02 15:45:37 +0800

    添加测试用例

--- test/index.ts
+++ test/index.ts
@@ -1,11 +1,24 @@
 import {ok,equal,deepStrictEqual, strictEqual} from 'assert';
 import fitBlock from '../index'
-let testUnit = {
+import level from 'fit-block-store'
+const server = level.getServer();
+const runBefore = {
+    [Symbol('before.server')] : async function() {
+        await server.listen();
+    },
+}
+const testUnit = {
     [Symbol('test.genGodBlock')] : async function() {
         await fitBlock.genGodBlock();
     },
     [Symbol('test.loadGodBlock')] : async function() {
         const godBlock = await fitBlock.loadGodBlock();
+    },
+}
+
+const runAfter = {
+    [Symbol('after.server')] : async function() {
+        await server.close();
     },
 }
 
@@ -16,4 +29,4 @@ async function run(testUnitList) {
         }
     }
 }
-run([testUnit]);
+run([runBefore, testUnit, runAfter]);
