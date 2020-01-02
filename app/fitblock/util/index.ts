275de192c2e6f7bd47f27946929d6d2680e9f17c
zy445566 <zyct@vip.qq.com>
2020-01-02 15:54:46 +0800

    修复存在问题

--- app/fitblock/util/index.ts
+++ app/fitblock/util/index.ts
@@ -1,8 +1,9 @@
 export function getRandHexNumByDigit(digitNum:number, radix:number):string
 {
-    const num = Math.round(Math.random()*(radix-1));
     const hexNums = [];
     for(let i = 0;i<digitNum;i++) {
+        let num = Math.round(Math.random()*(radix-1));
+        if(hexNums.length===0 && num===0){continue}
         hexNums.push(num.toString(radix));
     }
     return hexNums.join('');
