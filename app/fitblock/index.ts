14ce171c4eb689ebae9dc8e2a6d25e82dd46a633
zy445566 <zyct@vip.qq.com>
2020-01-10 15:03:52 +0800

    增加推导泛型

--- app/fitblock/index.ts
+++ app/fitblock/index.ts
@@ -15,7 +15,7 @@ export default class FitBlock extends AppBase {
         this.name = config.appName;
     }
 
-    getConfig():any {
+    getConfig() {
         return config;
     }
 
--- appFactory.ts
+++ appFactory.ts
@@ -1,14 +1,22 @@
 import AppBase from './types/AppBase';
-import fitblock from './app/fitblock';
-const classMap = {fitblock};
-const instanceMap: Map<String, AppBase> = new Map(); 
+import Fitblock from './app/fitblock';
+const instanceMap: Map<String, any> = new Map(); 
 export default class AppFactory {
-    static getAppByName(name: string): AppBase {
-        if(instanceMap.has(name)) {
-            return instanceMap.get(name);
+    static getAppByName(name: string) {
+        switch(name.toLowerCase()) {
+            case 'fitblock':
+                return AppFactory.getAppByClass<Fitblock>(Fitblock);
+            default:
+                throw new Error('not support app.')
         }
-        const appObj:AppBase = new classMap[name.toLowerCase()]();
-        instanceMap.set(name, appObj);
-        return appObj;
+    }
+
+    static getAppByClass<T extends AppBase>(app: new () => T):T {
+        if(instanceMap.has(app.name)) {
+            return instanceMap.get(app.name);
+        }
+        const appInstance = new app()
+        instanceMap.set(app.name, appInstance)
+        return appInstance;
     }
 }
